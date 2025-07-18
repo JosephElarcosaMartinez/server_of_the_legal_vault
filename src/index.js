c57314ba907babbd6388d0c722b4efb96a1fda67
Joseph Martinez <josephelarcosamartinez@gmai.com>
2025-07-18 12:21:35 +0800

    index

--- src/index.js
+++ src/index.js
@@ -24,12 +24,9 @@ app.use(
 app.use("/api", branchRoutes);
 app.use("/api", userRoutes);
 app.use("/api", authRoutes); // authentication api
-<<<<<<< HEAD
 app.use("/uploads", express.static("C:/Users/Lenovo i5 8th Gen/Desktop/CAPSTONE/uploads")); // user profile uploads 
-=======
-app.use("/uploads", express.static("D:/Capstone_ni_Angelie/uploads")); // user profile uploads
 app.use("/api", clientRoutes);
->>>>>>> bf1168663dba915e448d7e40430b2d06372d734e
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);

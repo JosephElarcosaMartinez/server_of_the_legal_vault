22f9eb2e702a3c4db178b5445d6a1373301fd70b
Joseph Martinez <josephelarcosamartinez@gmai.com>
2025-09-17 13:15:31 +0800

    js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import clientRoutes from "./routes/clientRoute.js";
 import caseRoutes from "./routes/caseRoute.js";
 import paymentRoutes from "./routes/paymentRoute.js";
 import taskRoutes from "./routes/taskRoute.js";
+import documentRoutes from "./routes/documentRoute.js";
 
 const app = express();
 const port = 3000;
